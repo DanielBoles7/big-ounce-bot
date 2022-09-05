508a7c318a5d829b1777fa297184d2c9e3814b94
DanielBoles7 <61218103+DanielBoles7@users.noreply.github.com>
2022-09-05 16:51:00 -0500

    Add files via upload

---
+++ bigouncebot.py
@@ -0,0 +1,30 @@
+import os
+import discord
+from dotenv import load_dotenv
+
+token = os.get_env("token")
+
+class bigOunce(discord.Client):
+    async def on_ready(self):
+        print(f'Logged on as {self.user}!')
+
+    async def on_message(self, message):
+        
+        print(f'Message from {message.author}: {message.content}')
+        
+        if message.author == client.user:
+            return
+
+        if("<@228347925183332352>" in message.content):
+            for number in range(5):
+                await message.channel.send('<@228347925183332352>')
+        
+        if("<@732432789949120542>" in message.content):
+            for number in range(5):
+                await message.channel.send('<@732432789949120542>')
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = bigOunce(intents=intents)
+client.run(token)
